977ee73de4d7d89762bd611e9042549913d97c64
deathwatchgaming <deathwatchgaming@gmail.com>
2024-10-03 15:42:57 -0700

    req mcc

--- Assets/VehiclesControl/SwatVan/SwatVan04/Scripts/SwatVan04Controller.cs
+++ Assets/VehiclesControl/SwatVan/SwatVan04/Scripts/SwatVan04Controller.cs
@@ -67,6 +67,10 @@ namespace VehiclesControl
 			// Rigidbody _rigidbody
 			[SerializeField] private Rigidbody _rigidbody;		
 
+			[Tooltip("The mesh collider component")]
+			// MeshCollider _meshCollider
+			[SerializeField] private MeshCollider _meshCollider;
+			
 		// Wheel Transforms
 		[Header("Wheel Transforms")]
 
@@ -161,7 +165,13 @@ namespace VehiclesControl
 			// Adjust the center of mass vertically to help prevent the SwatVan04 from rolling
 			// _rigidbody centerOfMass
 			_rigidbody.centerOfMass += Vector3.up * _centerOfGravityOffset;	
-	        
+
+			// _meshCollider GetComponent MeshCollider
+			_meshCollider = GetComponent<MeshCollider>();
+
+			// _meshCollider convex
+			_meshCollider.convex = true;
+
 		} // close private void Awake
 		
 		// private void Update
